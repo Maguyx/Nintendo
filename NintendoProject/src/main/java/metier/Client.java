8b7dc657851758e23703e080820e46b86e29419f
amaury <amaury.foulonneau@gmail.com>
2021-03-12 16:24:24 +0100

    update classe Client

--- NintendoProject/src/main/java/metier/Client.java
+++ NintendoProject/src/main/java/metier/Client.java
@@ -6,5 +6,42 @@ import java.util.List;
 public class Client {
 	private String nom;
 	private String prenom;
-	private List<Jeu> listJeu= new ArrayList();
+	private List<Jeu> listDeJeu= new ArrayList();
+	
+	
+	
+	public Client() {
+	}
+	
+	public Client(String nom, String prenom, List<Jeu> listDeJeu) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.listDeJeu = listDeJeu;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public List<Jeu> getListDeJeu() {
+		return listDeJeu;
+	}
+	public void setListDeJeu(List<Jeu> listDeJeu) {
+		this.listDeJeu = listDeJeu;
+	}
+	
+	@Override
+	public String toString() {
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", listDeJeu=" + listDeJeu + "]";
+	}
+	
+	
 }
