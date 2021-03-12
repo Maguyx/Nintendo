86f9df6b952f140e21c5020f4533f024956a8daf
amaury <amaury.foulonneau@gmail.com>
2021-03-12 16:16:48 +0100

    add client class

---
+++ NintendoProject/src/main/java/metier/Client.java
@@ -0,0 +1,10 @@
+package metier;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+	private String nom;
+	private String prenom;
+	private List<Jeu> listJeu= new ArrayList();
+}
