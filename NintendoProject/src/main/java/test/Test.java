0c53b0ac4a0b62210c1d16a3ae52dc006c5120bb
Morgan <morgan.larivain@yahoo.fr>
2021-03-12 16:59:20 +0100

    etape 2

--- NintendoProject/src/main/java/test/Test.java
+++ NintendoProject/src/main/java/test/Test.java
@@ -1,9 +1,36 @@
+
+
 package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import metier.Console;
+import metier.Jeu;
+
 
 public class Test {
 
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<Console> nintendo =new ArrayList<Console>();
+		
+		Console ps4 = new Console ("PS4");
+		Console wii = new Console ("WII");
+		Console xbox = new Console ("XBOX");
+		Console pc = new Console ("PC");
+		nintendo.add(ps4);
+		nintendo.add(wii);
+		
+		Jeu ac= new Jeu("Assassin's Creed", nintendo);
+		Jeu fifa=new Jeu("Fifa", nintendo);
+		Jeu witcher= new Jeu("The Witcher", nintendo);
+		Jeu horizon = new Jeu("Horizon Zero Dawn", nintendo);
+		Jeu lol = new Jeu("League of Legends", nintendo);
+		
 
 	}
 
